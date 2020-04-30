3a32b625af9fae66755ace080b51785fc6bcd7c8
R. Springer <r.springer@rocmondriaan.nl>
2020-04-30 22:33:49 +0200

    First webpack setup

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+require('../css/style.scss');
+require('../css/animation.scss');
+
+let contactForm = document.getElementById("contact-form");
+contactForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let formData = new FormData(contactForm);
+    if (formData.has("name") && formData.has("subject") && formData.has("body")) {
+        let hasValues = true;
+        for (let value of formData.values()) {
+            if (value.length == 0) {
+                hasValues = false;
+            }
+        }
+        if (hasValues) {
+            window.open(`mailto:sociallifeinstantperformance@gmail.com?subject=${encodeURI(formData.get("subject"))}&body=${encodeURI(formData.get("body") + "\n\nMet Vriendelijke groet,\n\n")}${formData.get("name")}`);
+        }
+    }
+});
---
+++ webpack.common.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        app: './src/js/index.js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+              postcss: [
+                autoprefixer(),
+              ]
+            }
+          }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Slip B.V.',
+            template: './src/index.html'
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                    'postcss-loader'
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: () => [autoprefixer()]
+                    }
+                }]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader',
+                ],
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader',
+                ],
+            },
+        ],
+    },
+}
